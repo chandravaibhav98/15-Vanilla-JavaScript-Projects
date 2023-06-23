6102fa3bdc47c83ffa4c946e9afcece315fc59fd
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-06-24 01:16:26 +0530

    24/6
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- color-flipper/main.js
+++ color-flipper/main.js
@@ -11,5 +11,5 @@ btn.addEventListener('click', function () {
 	color.textContent = colors[randomNumber];
 });
 function getRandomNumber() {
-	return Math.random() * colors.length;
+	return Math.floor(Math.random() * colors.length);
 }
