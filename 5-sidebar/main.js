488f0d391cc5202fb97dd0672526a1853e9ad440
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-06-26 17:05:58 +0530

    Start Modal
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- 5-sidebar/main.js
+++ 5-sidebar/main.js
@@ -1,24 +1,18 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const toggleBtn = document.querySelector('.sidebar-toggle');
+const closeBtn = document.querySelector('.close-btn');
+const sidebar = document.querySelector('.sidebar');
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+toggleBtn.addEventListener('click', function () {
+	/* 
+    if (sidebar.classList.contains('show-sidebar')) {
+		sidebar.classList.remove('show-sidebar');
+	} else {
+		sidebar.classList.add('show-sidebar');
+    }
+    */
+	sidebar.classList.toggle('show-sidebar');
+});
 
-setupCounter(document.querySelector('#counter'))
+closeBtn.addEventListener('click', function () {
+	sidebar.classList.remove('show-sidebar');
+});
