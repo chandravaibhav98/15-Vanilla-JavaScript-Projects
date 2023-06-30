f46b252c7e1308c68ce03ae31a4fa39d41f992ac
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-07-01 00:19:58 +0530

    Questions -Selectors and Traversing DOM
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- 7-questions/main.js
+++ 7-questions/main.js
@@ -1,13 +1,30 @@
 import './style.css';
 
 // using selectors inside the element
-// traversing the DOM
-
-const btns = document.querySelectorAll('.question-btn');
-btns.forEach(function (btn) {
-	btn.addEventListener('click', function (e) {
-		console.log(e.currentTarget.parentElement.parentElement);
-		const question = e.currentTarget.parentElement.parentElement;
+const questions = document.querySelectorAll('.question');
+questions.forEach(function (question) {
+	console.log(question);
+	const btn = question.querySelector('.question-btn');
+	btn.addEventListener('click', function () {
+		// close other questions
+		questions.forEach(function (item) {
+			console.log(item);
+			if (item !== question) {
+				item.classList.remove('show-text');
+			}
+		});
 		question.classList.toggle('show-text');
 	});
 });
+
+// traversing the DOM
+/* 
+const btns = document.querySelectorAll('.question-btn');
+btns.forEach(function (btn) {
+	btn.addEventListener('click', function (button) {
+		console.log(button.currentTarget.parentElement.parentElement);
+		const question = button.currentTarget.parentElement.parentElement;
+		question.classList.toggle('show-text');
+	});
+});
+*/
