43389e5ff0aad4bc892706e1b8a49b8389092912
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-07-03 02:58:52 +0530

    Naive
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- 8-menu/main.js
+++ 8-menu/main.js
@@ -1,24 +1,128 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const menu = [
+	{
+		id: 1,
+		title: 'buttermilk pancakes',
+		category: 'breakfast',
+		price: 15.99,
+		img: './images/item-1.jpeg',
+		desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+	},
+	{
+		id: 2,
+		title: 'diner double',
+		category: 'lunch',
+		price: 13.99,
+		img: './images/item-2.jpeg',
+		desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+	},
+	{
+		id: 3,
+		title: 'godzilla milkshake',
+		category: 'shakes',
+		price: 6.99,
+		img: './images/item-3.jpeg',
+		desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+	},
+	{
+		id: 4,
+		title: 'country delight',
+		category: 'breakfast',
+		price: 20.99,
+		img: './images/item-4.jpeg',
+		desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+	},
+	{
+		id: 5,
+		title: 'egg attack',
+		category: 'lunch',
+		price: 22.99,
+		img: './images/item-5.jpeg',
+		desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+	},
+	{
+		id: 6,
+		title: 'oreo dream',
+		category: 'shakes',
+		price: 18.99,
+		img: './images/item-6.jpeg',
+		desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+	},
+	{
+		id: 7,
+		title: 'bacon overflow',
+		category: 'breakfast',
+		price: 8.99,
+		img: './images/item-7.jpeg',
+		desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+	},
+	{
+		id: 8,
+		title: 'american classic',
+		category: 'lunch',
+		price: 12.99,
+		img: './images/item-8.jpeg',
+		desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+	},
+	{
+		id: 9,
+		title: 'quarantine buddy',
+		category: 'shakes',
+		price: 16.99,
+		img: './images/item-9.jpeg',
+		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+	},
+	{
+		id: 10,
+		title: 'bison steak',
+		category: 'dinner',
+		price: 22.99,
+		img: './images/item-10.jpeg',
+		desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+	},
+];
 
-setupCounter(document.querySelector('#counter'))
+const sectionCenter = document.querySelector('.section-center');
+const filterBtns = document.querySelectorAll('.filter-btn');
+
+// Load Items
+window.addEventListener('DOMContentLoaded', function () {
+	displayMenuItems(menu);
+});
+
+// Filter Items
+filterBtns.forEach(function (btn) {
+	btn.addEventListener('click', function (e) {
+		const category = e.currentTarget.dataset.id;
+		const menuCategory = menu.filter(function (menuItem) {
+			if (menuItem.category == category) return menuItem;
+		});
+		// console.log(menuCategory);
+		if (category === 'all') {
+			displayMenuItems(menu);
+		} else {
+			displayMenuItems(menuCategory);
+		}
+	});
+});
+
+function displayMenuItems(menuItems) {
+	let displayMenu = menuItems.map(function (item) {
+		return `<article class="menu-item">
+                    <img src="${item.img}" class="photo" alt="${item.title}">
+                    <div class="item-info">
+                        <header>
+                            <h4>${item.title}</h4>
+                            <h5 class="price">${item.price}</h5>
+                        </header>
+                        <p class="item-text">
+                            ${item.desc} 
+                        </p>
+                    </div>
+                </article>`;
+	});
+	displayMenu = displayMenu.join('');
+	console.log(displayMenu);
+	sectionCenter.innerHTML = displayMenu;
+}
