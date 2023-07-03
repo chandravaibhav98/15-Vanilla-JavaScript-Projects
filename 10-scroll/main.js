75408417cd2d5adcc941cfe394332234d010519b
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-07-04 01:31:56 +0530

    Tabs init
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- 10-scroll/main.js
+++ 10-scroll/main.js
@@ -1,24 +1,86 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
+// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// slice extracts a section of a string without modifying original string
+// offsetTop - A Number, representing the top position of the element, in pixels
 
-setupCounter(document.querySelector('#counter'))
+// ********** set date ************
+// select span
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
+
+// ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function () {
+	// linksContainer.classList.toggle("show-links");
+
+	const linksHeight = links.getBoundingClientRect().height;
+	const containerHeight = linksContainer.getBoundingClientRect().height;
+	if (containerHeight === 0) {
+		linksContainer.style.height = `${linksHeight}px`;
+	} else {
+		linksContainer.style.height = 0;
+	}
+	// console.log(linksContainer.getBoundingClientRect());
+});
+
+// ********** fixed navbar ************
+
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
+
+window.addEventListener('scroll', function () {
+	const scrollHeight = window.pageYOffset;
+	const navHeight = navbar.getBoundingClientRect().height;
+	if (scrollHeight > navHeight) {
+		navbar.classList.add('fixed-nav');
+	} else {
+		navbar.classList.remove('fixed-nav');
+	}
+	// setup back to top link
+
+	if (scrollHeight > 500) {
+		console.log('helo');
+
+		topLink.classList.add('show-link');
+	} else {
+		topLink.classList.remove('show-link');
+	}
+});
+
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll('.scroll-link');
+scrollLinks.forEach((link) => {
+	link.addEventListener('click', (e) => {
+		// prevent default
+		e.preventDefault();
+		// navigate to specific spot
+		const id = e.currentTarget.getAttribute('href').slice(1);
+		const element = document.getElementById(id);
+
+		const navHeight = navbar.getBoundingClientRect().height;
+		const containerHeight = linksContainer.getBoundingClientRect().height;
+		const fixedNav = navbar.classList.contains('fixed-nav');
+		let position = element.offsetTop - navHeight;
+
+		if (!fixedNav) {
+			position = position - navHeight;
+		}
+		if (navHeight > 82) {
+			position = position + containerHeight;
+		}
+
+		window.scrollTo({
+			left: 0,
+			top: position,
+		});
+		// close
+		linksContainer.style.height = 0;
+	});
+});
+// calculate heights
