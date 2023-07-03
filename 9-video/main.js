be4a9268f6af331374c98e1e481a29df7e6dff7e
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-07-03 22:54:07 +0530

    Scroll init
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- 9-video/main.js
+++ 9-video/main.js
@@ -1,24 +1,25 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// MDN
+// The DOMContentLoaded event fires when the initial HTML document has been completely loaded and parsed, without waiting for stylesheets, images and sub-frames to finish loading
+// The load event is fired when the whole page has loaded, including all dependent resources such as stylesheets and images
 
-setupCounter(document.querySelector('#counter'))
+const btn = document.querySelector('.switch-btn');
+const video = document.querySelector('.video-container');
+
+btn.addEventListener('click', function () {
+	if (!btn.classList.contains('slide')) {
+		btn.classList.add('slide');
+		video.pause();
+	} else {
+		btn.classList.remove('slide');
+		video.play();
+	}
+});
+
+// preloader
+const preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', function () {
+	preloader.classList.add('hide-preloader');
+});
