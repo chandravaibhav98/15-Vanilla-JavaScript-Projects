f73d9a9a993506f432bee8030cc68373133cb01a
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-07-04 15:01:13 +0530

    html div/section
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- 12-countdown-timer/main.js
+++ 12-countdown-timer/main.js
@@ -46,3 +46,41 @@ const weekday = weekdays[futureDate.getDay()];
 console.log(weekday);
 
 giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${year} ${hours}: ${minutes}`;
+
+// future time in ms
+const futureTime = futureDate.getTime();
+console.log(futureTime);
+
+function getRemainingTime() {
+	const today = new Date().getTime();
+	const t = futureTime - today;
+	// console.log(t);
+
+	// values in ms : 1s = 1000ms; 1m = 60s; 1hr = 60mins; 1D = 24hr
+	const oneDay = 24 * 60 * 60 * 1000;
+	const oneHour = 60 * 60 * 1000;
+	const oneMinute = 60 * 1000;
+
+	// calculate all values
+	let days = t / oneDay;
+	days = Math.floor(days); // convert to int
+	// console.log(days);
+
+	let hours = Math.floor((t % oneDay) / oneHour);
+	let minutes = Math.floor((t % oneHour) / oneMinute);
+	let seconds = Math.floor((t % oneMinute) / 1000);
+
+	// set values array
+	const values = [days, hours, minutes, seconds];
+
+	function format(item) {
+		if (item < 10) {
+			return (item = `0${item}`);
+		}
+		return item;
+	}
+	items.forEach(function (item, index) {
+		item.innerHTML = format(values[index]);
+	});
+}
+getRemainingTime();
