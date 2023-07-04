a5f4659474c49cd99e977bd4aa96554a50d61469
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-07-04 10:46:43 +0530

    14 moveoutof forEach() : add('active')
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- 11-tabs/main.js
+++ 11-tabs/main.js
@@ -1,18 +1,17 @@
 import './style.css';
 
-const btns = document.querySelectorAll('.tab-btn');
 const about = document.querySelector('.about');
+const btns = document.querySelectorAll('.tab-btn');
 const articles = document.querySelectorAll('.content');
-
 about.addEventListener('click', function (e) {
-	// console.log(e.target.dataset.id);
 	const id = e.target.dataset.id;
+	console.log(id);
 	if (id) {
-		// remove active from other buttons
+		// remove selected from other buttons
 		btns.forEach(function (btn) {
-			btns.classList.remove('active');
-			e.target.classList.add('active');
+			btn.classList.remove('active');
 		});
+		e.target.classList.add('active');
 		// hide other articles
 		articles.forEach(function (article) {
 			article.classList.remove('active');
