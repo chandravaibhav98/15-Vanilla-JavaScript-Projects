c67656d97ab41919ac516b2e07437a6638283516
chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>
2023-07-05 21:32:06 +0530

    19
    Signed-off-by: chandravaibhav98 <78618825+chandravaibhav98@users.noreply.github.com>

--- 19-darkmode/main.js
+++ 19-darkmode/main.js
@@ -1,17 +1,15 @@
-import './style.css';
-
 const toggleBtn = document.querySelector('.btn');
 const articlesContainer = document.querySelector('.articles');
 
 toggleBtn.addEventListener('click', () => {
-	document.documentElement.classList.toggle('dark-theme');
+  document.documentElement.classList.toggle('dark-theme');
 });
 
 const articlesData = articles
-	.map((article) => {
-		const { title, date, length, snippet } = article;
-		const formatDate = moment(date).format('MMMM Do, YYYY');
-		return `<article class="post">
+  .map((article) => {
+    const { title, date, length, snippet } = article;
+    const formatDate = moment(date).format('MMMM Do, YYYY');
+    return `<article class="post">
           <h2>${title}</h2>
           <div class="post-info">
             <span>${formatDate}</span>
@@ -21,7 +19,7 @@ const articlesData = articles
             ${snippet}
           </p>
         </article>`;
-	})
-	.join('');
+  })
+  .join('');
 
 articlesContainer.innerHTML = articlesData;
